5fb5df4c160379f409e6174a3a7ae10716e85254
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-12 17:42:55 -0500

    update with try except KeyError

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ data = pd.read_csv('assets/nato_phonetic_alphabet.csv')
 nato = {row['letter']:row['code'] for (index,row) in data.iterrows()}
 # print(nato)
 
-user_word = input('\nPlease enter a word: ').upper()
+def generate_phonetic():
+    user_word = input('\nPlease enter a word: ').upper()
+    try:
+        nato_output = [nato[letter] for letter in user_word]
+    except KeyError:
+        print('\nSorry, only letters in the alphabet please.\n')
+        generate_phonetic()
+    else:
+        print(f'\n{nato_output}\n')
 
-nato_output = [nato[letter] for letter in user_word if letter in nato.keys()]
-print(f'\n{nato_output}\n')
+generate_phonetic()
+
+
+# or
+# user_word = input('\nPlease enter a word: ').upper()
+# nato_output = [nato[letter] for letter in user_word if letter in nato.keys()]
+# print(f'\n{nato_output}\n')
